d4c0fff010c98fb7c25c79e0ccd0c08168b5535d
Medha Soman <medhasoman13@gmail.com>
2024-05-23 11:26:25 +0100

    Added @Repository annotation to repository classes
    - Included missing @Repository annotation to ensure proper Spring component scanning

--- src/main/java/com/med/banking/repository/TransactionRepository.java
+++ src/main/java/com/med/banking/repository/TransactionRepository.java
@@ -2,9 +2,11 @@ package com.med.banking.repository;
 
 import com.med.banking.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
  List<Transaction> findByAccountIdOrderByTimestampDesc( Long accountId);
 }
